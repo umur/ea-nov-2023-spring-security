c9c027423c0e71ebd809bbd5acf0c96b45bb71a5
MOAID <moaidmoaidrazhy@gmail.com>
2023-11-13 20:13:55 -0600

    Revert "Initial commit"
    This reverts commit 39ed061c1226b486470a9c6b1b28fc65a6deba46.

--- src/main/java/edu/miu/springsecurity1/aspect/annotation/LogMe.java
+++
@@ -1,8 +0,0 @@
-package edu.miu.springsecurity1.aspect.annotation;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target(ElementType.METHOD)
-public @interface LogMe {
-}
--- src/main/java/edu/miu/springsecurity1/repository/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package edu.miu.springsecurity1.repository;
-
-import edu.miu.springsecurity1.entity.Product;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepo extends CrudRepository<Product, Integer> {
-}
